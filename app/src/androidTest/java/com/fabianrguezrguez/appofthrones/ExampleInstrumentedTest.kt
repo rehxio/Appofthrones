60ddbbb6cc45d5535132c81b5c5c5170b5643bda
Fabián Rodríguez Rodríguez <fabianrguezrguez@gmail.com>
2018-10-22 09:04:59 +0100

    Added test and start add mock

--- app/src/androidTest/java/com/fabianrguezrguez/appofthrones/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fabianrguezrguez/appofthrones/ExampleInstrumentedTest.kt
@@ -7,6 +7,22 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import android.support.test.filters.LargeTest
+import com.fabianrguezrguez.appofthrones.R.id.googleBtn
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.rule.BaristaRule
+import org.junit.Before
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+
+
+
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,8 +33,45 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.fabianrguezrguez.appofthrones", appContext.packageName)
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HelloWorldEspressoTest {
+
+    @get:Rule
+    var baristaRule = BaristaRule.create(MainActivity::class.java)
+
+    @Rule
+    var wireMockRule = WireMockRule(8089)
+
+    @Before
+    fun setup() {
+        baristaRule.launchActivity()
+    }
+
+    @Test
+    fun listGoesOverTheFold() {
+        assertDisplayed("Jon Snow")
+    }
+
+    @Test
+    fun openUrlLinkOnNewWindow() {
+
+        val server = MockWebServer()
+
+        server.enqueue(MockResponse().setBody("hello, world!"))
+
+        server.start()
+
+        clickOn(googleBtn)
+        val request1 = server.takeRequest()
+        assertEquals("https://www.google.com", request1.path)
+        assertNotNull(request1.getHeader("Authorization"))
+
+        server.shutdown()
     }
 }
--- app/src/main/java/com/fabianrguezrguez/appofthrones/MainActivity.kt
+++ app/src/main/java/com/fabianrguezrguez/appofthrones/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.fabianrguezrguez.appofthrones
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        googleBtn.setOnClickListener {
+            val client = OkHttpClient()
+
+            val request = Request.Builder()
+                .url("https://www.leanmind.es")
+                .build()
+
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        padre.text = "Failure !"
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body()?.string()
+                    runOnUiThread {
+                        try {
+                            padre.text = body
+                        } catch (ioe: IOException) {
+                            padre.text = "Error during get body"
+                        }
+                    }
+                }
+            })
+
+        }
     }
 }
